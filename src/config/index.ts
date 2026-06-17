7934d5dec0d3352fbc8fe1dc729b7665c135e03a
Yifei Wu <8293149+yifeiwu@users.noreply.github.com>
2026-06-17 11:05:10 +1000

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
+  title: "Alejandro Múnez",
   author: "Alejandro Múnez Cuntez",
   description:
     "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
